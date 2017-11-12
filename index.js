617e20858e544ff65c7bd3f8dc30013017e02fb0
Antonio Dell'Ava <antonio.dellava@gmail.com>
2017-11-12 19:22:46 +0100

    components saved ordered by price :sparkles:

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var Crawler = require("crawler");
+var jsonfile = require('jsonfile')
 const resources = require('./resources.json');
 
-const c = new Crawler();
+const c = new Crawler({
+    callback: function (error, res, done) {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log("final cb");
+        }
+        done();
+    }
+});
 
 const sanitize = (c) => {
     if (!c) return "0.00";
@@ -12,13 +22,36 @@ const sanitize = (c) => {
     return n;
 };
 
-const cbGenerator = (schema, origin) => {
+const cbGenerator = (schema, origin, componentKey, componentTypeKey) => {
     return (error, res, done) => {
         if (error) {
             console.log(error);
         } else {
             var $ = res.$;
             console.log("   - prezzo " + origin + " ", sanitize($(schema).text()));
+
+            if (!objToBeSaved[componentTypeKey]){
+                objToBeSaved[componentTypeKey] = {};
+            }
+
+            if (!objToBeSaved[componentTypeKey][componentKey]) {
+                objToBeSaved[componentTypeKey][componentKey] = [];
+            }
+
+            objToBeSaved[componentTypeKey][componentKey].push({
+                origin: origin,
+                price: sanitize($(schema).text())
+            });
+
+            objToBeSaved[componentTypeKey][componentKey].sort(item => -item.price);
+
+            jsonfile.writeFile("data.json", objToBeSaved, {
+                spaces: 4
+            }, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
         }
         done();
     };
@@ -27,15 +60,16 @@ const cbGenerator = (schema, origin) => {
 
 
 let arrToBeParsed = [];
+let objToBeSaved = {};
 
-Object.entries(resources.components).forEach(([componentTypeKey, componentTypeValue]) => {
+Object.entries(resources.components).forEach(([componentTypeKey, componentTypeValue], i) => {
     console.log(componentTypeKey);
     Object.entries(resources.components[componentTypeKey]).forEach(([componentKey, componentValue]) => {
         console.log(">>"+componentKey);
         arrToBeParsed = arrToBeParsed.concat(resources.components[componentTypeKey][componentKey].options.reduce((acc, val) => {
             acc.push({
                 uri: val.URL,
-                callback: cbGenerator(resources.schemas[val.schema], val.schema)
+                callback: cbGenerator(resources.schemas[val.schema], val.schema, componentKey, componentTypeKey)
             });
             return acc;
         }, []));
